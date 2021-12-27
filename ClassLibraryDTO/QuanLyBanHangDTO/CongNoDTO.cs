2deb3fa9a3de1e4cf0f00b7c3a403918939960f8
Ngô Võ Tuấn An <ngovotuananh@gmail.com>
2021-12-27 13:22:30 +0700

    Second commit

--- ClassLibraryDTO/QuanLyBanHangDTO/CongNoDTO.cs
+++ ClassLibraryDTO/QuanLyBanHangDTO/CongNoDTO.cs
@@ -8,6 +8,8 @@ namespace ClassLibraryDTO.QuanLyBanHangDTO
 {
     public class CongNoDTO
     {
+        //testasdg
+        //dawgsds
         public string MaKhachHang{ get; set; }
         public string TenKhachHang { get; set; }
         public string SoDienThoai { get; set; }
